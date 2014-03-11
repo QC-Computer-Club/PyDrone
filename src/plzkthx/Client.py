b9bc72a1f36e4ce72316718302740abb3e76e427
zim <zim@Zim-Fenrir>
2014-03-11 00:31:38 -0400

    client

---
+++ src/plzkthx/Client.py
@@ -0,0 +1,17 @@
+'''
+Created on Mar 11, 2014
+
+@author: zim
+'''
+
+class Client(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
