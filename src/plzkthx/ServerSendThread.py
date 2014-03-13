c403519f6069d1a1c2b351698e1a6439e8e8be58
zim <zim@Zim-Fenrir>
2014-03-12 22:49:48 -0400

    SendThread WIP

--- src/plzkthx/ServerSendThread.py
+++ src/plzkthx/ServerSendThread.py
@@ -4,14 +4,22 @@ Created on Mar 11, 2014
 @author: zim
 '''
 
-class ServerSendThread(object):
+import socket
+import threading
+import time
+
+class ServerSendThread(threading.Thread):
     '''
     classdocs
     '''
-
-
-    def __init__(self, params):
+    def __init__(self, clientsocket, clientaddress, webcam):
         '''
         Constructor
         '''
+        threading.Thread.__init__(self)
+        self.socket = clientsocket
+        self.address = clientaddress
         
+    def run(self):
+        while True:
+            time.sleep(1)
